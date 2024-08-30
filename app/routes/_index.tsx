6d44c0e10204cd5311b90906a510b80da7cc09ab
Hullen Gonzales <hullen@hullen.me>
2024-08-29 21:41:38 -0300

    feat: :lipstick: update UI based on figma layout, with some improvements on UX

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useState } from "react";
+import useNotification from "../hooks/useNotification";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +11,9 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   const [imageUrl, setImageUrl] = useState<string>('');
+  const [buttonText, setButtonText] = useState<string>('Copiar');
   const [text, setText] = useState('')
+  const showNotification = useNotification();
 
   const handleClick = () => {
     if (text && text.trim().length > 0) {
@@ -27,47 +30,56 @@ export default function Index() {
       
       const clipboardItem = new ClipboardItem({ "image/png": blob });
       await navigator.clipboard.write([clipboardItem]);
-
-      alert("Imagem copiada para o clipboard!");
+      setButtonText('Copiado!');
+      setTimeout(() => {
+          setButtonText('Copiar');
+      }, 2000);
+      showNotification("Imagem copiada para o clipboard!");
     } catch (error) {
       console.error("Erro ao copiar a imagem: ", error);
     }
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">Tá aqui Generator</h1>
-      <input
-        type="text"
-        name="text"
-        value={text}
-        onChange={(v) => setText(v.target.value)}
-        placeholder="Digite o texto"
-        required
-        className="w-full max-w-md p-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <button
-        onClick={handleClick}
-        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors duration-200"
-      >
-        Gerar Imagem
-      </button>
+    <div className="flex w-full justify-center min-h-screen p-4">
+      <div className="flex flex-col items-center w-full max-w-md">
+      <div className="mb-2">
+        <img src="/assets/taqui-a-logo.png" alt="Tá aqui Generator" className="w-auto m-auto" />
+      </div>
+      <div className="flex w-full my-4 gap-4">
+        <input
+          type="text"
+          name="text"
+          value={text}
+          onChange={(v) => setText(v.target.value)}
+          placeholder="Digite aqui o contexto"
+          required
+          className="w-full p-3 text-2xl border-[3px] border-black rounded-md focus:outline-none focus:shadow-[3px_3px_0px_0px_#000000] transition-shadow duration-200"
+        />
+        <button
+          onClick={handleClick}
+          className="py-3 px-6 text-2xl text-black font-bold rounded-md bg-[#FFE500] hover:bg-[#FFF129] hover:shadow-[3px_3px_0px_0px_#000000] active:bg-[#FFE500] active:shadow-[3px_3px_0px_0px_#000000] border-[3px] border-black transition duration-200 focus:outline-none"
+        >
+          Gerar
+        </button>
+      </div>
       {imageUrl && (
-        <div className="mt-6 flex flex-col items-center">
+        <div className="mt-4 flex flex-col items-center max-w-md gap-4">
           <img
             id="npm-install"
             src={imageUrl}
             alt="Imagem gerada"
-            className="w-full max-w-md border border-gray-300 rounded-md shadow-sm"
+            className="w-full border border-gray-300 rounded-md shadow-sm"
           />
           <button
             onClick={copy}
-            className="mt-4 bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition-colors duration-200"
+            className="py-3 px-6 text-2xl text-black font-bold bg-[#FFE500] hover:bg-[#FFF129] hover:shadow-[3px_3px_0px_0px_#000000] active:bg-[#FFE500] active:shadow-[3px_3px_0px_0px_#000000] rounded-md border-[3px] border-black transition duration-200 focus:outline-none"
           >
-            Copiar
+            {buttonText}
           </button>
         </div>
       )}
+      </div>
     </div>
   );
 }
