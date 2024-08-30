ea1d76e1b2bc080bd5923b48078ee0170b0db4fe
Hullen Gonzales <hullen@hullen.me>
2024-08-29 21:28:34 -0300

    feat: :lipstick: add project background image

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-[url('/assets/taqui-o-background.png')] bg-center bg-no-repeat bg-cover">
         {children}
         <ScrollRestoration />
         <Scripts />
