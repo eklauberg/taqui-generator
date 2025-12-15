5cab5d9060fffd0e7740df2f894c7fc28739f426
eklauberg <eklauberg@geomais.com.br>
2025-12-15 20:04:29 -0300

    feat: adicionar pagina do WhatsApp com funcionalidade de gerar link para conversa

---
+++ app/routes/whatsapp.tsx
@@ -0,0 +1,7 @@
+import { WhatsappPage, whatsappMeta } from "../features/whatsapp/WhatsappPage";
+
+export const meta = whatsappMeta;
+
+export default function WhatsappRoute() {
+	return <WhatsappPage />;
+}
